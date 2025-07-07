0cfaa12206e32531c1e2ccb5bd310d59141d60c5
Nishant Singh <nishaantkrsingh@gmail.com>
2025-07-07 14:51:33 +0530

    minor changes

--- extension/popup.js
+++ extension/popup.js
@@ -1,12 +1,13 @@
 // popup.js
 document.getElementById('activateReader').addEventListener('click', () => {
-    // Get the current active tab
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         const activeTab = tabs[0];
-        // Execute the content script in the active tab
+
+        // Inject both scripts. 'marked.min.js' will run first,
+        // making the 'marked' object available to 'content.js'.
         chrome.scripting.executeScript({
             target: { tabId: activeTab.id },
-            files: ['content.js']
+            files: ['libs/marked.min.js', 'content.js']
         });
     });
 });
